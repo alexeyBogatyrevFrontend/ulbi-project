9f51877257cc6792725bd79739471ad08ecf8089
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-10-30 22:24:36 +0500

    fix: change svgr

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,10 +1,10 @@
+/// <reference types="vite-plugin-svgr/client" />
+
 import { Theme, useTheme } from '#src/app/providers/ThemeProvider/index.tsx'
 import { classNames } from '#src/shared/lib/classNames/classNames.ts'
 import { FC } from 'react'
 
-// @ts-ignore
 import LightIcon from '#src/shared/assets/icons/theme-light.svg?react'
-// @ts-ignore
 import DarkIcon from '#src/shared/assets/icons/theme-dark.svg?react'
 
 import cls from './ThemeSwitcher.module.scss'
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import svgr from 'vite-plugin-svgr'
 
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
